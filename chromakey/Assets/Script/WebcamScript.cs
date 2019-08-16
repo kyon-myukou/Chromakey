f718f226d403dac5ee76c5761c61374a78f42330
kyon-myukou <myukou@gmail.com>
2019-08-16 11:10:45 +0900

    2019/6/14

---
+++ chromakey/Assets/Script/WebcamScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebcamScript : MonoBehaviour
+{
+  int width = 1920;
+  int height = 1080;
+  int fps = 30;
+  WebCamTexture webcamTexture;
+
+  void Start () {
+    WebCamDevice[] devices = WebCamTexture.devices;
+    webcamTexture = new WebCamTexture(devices[0].name, this.width, this.height, this.fps);
+    GetComponent<Renderer> ().material.mainTexture = webcamTexture;
+    webcamTexture.Play();
+
+    GameObject obj = GameObject.Find("Camera");
+    Vector3 scale = obj.transform.localScale;
+    scale.x = -scale.x;
+    obj.transform.localScale = scale;
+  }
+}
